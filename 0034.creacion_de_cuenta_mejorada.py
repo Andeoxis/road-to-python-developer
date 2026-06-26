c14f3c1ac91b15bdb8fea6243c10fdf5a3e7b433
Anthony Leandro Vasquez Torrico <vasqueztorricoanthonyleandro@gmail.com>
2026-06-25 22:24:27 -0400

    learning for

---
+++ 0034.creacion_de_cuenta_mejorada.py
@@ -0,0 +1,7 @@
+# Queremos simular el conteo de días para una incubadora comercial (18 días)
+print("Iniciando incubación...")
+
+for dia in range(1, 19):  # range(1, 19) va desde el 1 hasta el 18 (el último no lo cuenta)
+    print(f"Día {dia}: Manteniendo temperatura ideal. 🌡️")
+
+print("¡Llegó el día 18! Los pollitos están naciendo. 🐣")
