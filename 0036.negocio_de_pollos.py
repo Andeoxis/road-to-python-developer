78b89e7fd6219f5fb7253cdfa812ec0e8dd096d2
Anthony Leandro Vasquez Torrico <vasqueztorricoanthonyleandro@gmail.com>
2026-06-25 22:32:20 -0400

    third example the for

---
+++ 0036.negocio_de_pollos.py
@@ -0,0 +1,8 @@
+print("--- SISTEMA DE DESPACHO DE CAMIONES ---")
+
+# Le decimos al for que cuente del 1 al 4
+for camion in range(1, 5):  # Recuerda que range termina uno antes, por eso ponemos 5
+    print(f"🚛 Camión N° {camion}: Cargando cajas de pollo y huevo...")
+    print(f"✅ Camión N° {camion}: ¡Datos verificados! Buen viaje, despacho autorizado.\n")
+
+print("⚡ Todos los camiones del día han sido despachados con éxito.")
