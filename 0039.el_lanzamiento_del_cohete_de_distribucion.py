734e813f6d0d190bd34a15a7dd757ad15c86d149
Anthony Leandro Vasquez Torrico <vasqueztorricoanthonyleandro@gmail.com>
2026-06-25 23:36:32 -0400

    for

---
+++ 0039.el_lanzamiento_del_cohete_de_distribucion.py
@@ -0,0 +1,8 @@
+# Imagina que estás automatizando el sistema de alertas de despacho express para los pedidos grandes de huevos en tu negocio.
+# Antes de liberar el camión, el sistema debe mostrar una cuenta regresiva de seguridad en la pantalla.
+
+print('\n---------- SISTEMA DE AUTOMOTIZACION DE ALERTAS DE DESPACHO EXPRESS ----------\n')
+
+for despacho in range(5, 0, -1):
+    print(f'🚨 Despacho en: {despacho}...')
+print(f'🚚 BOOM! Camion despachado. Los pollos y huevos van en camino, buen viaje.')
