a6909cc9478aaf7a4ae2b1e47e447dcd73a10079
Anthony Leandro Vasquez Torrico <vasqueztorricoanthonyleandro@gmail.com>
2026-07-18 19:01:56 -0400

    practica2

---
+++ 0083.practicando_def2.py
@@ -0,0 +1,12 @@
+def mostrar_nombre(tu_nombre):
+    print(f'Tu nombre es: {tu_nombre}')
+
+def mostrar_edad(tu_edad):
+    print(f'Tu edad es: {tu_edad}')
+
+nombre = input('Ingresa tu nombre: ')
+edad = input('Ingresa tu edad: ')
+
+mostrar_nombre(nombre)
+mostrar_edad(edad)
+
