7c1668c4c8670bce23773844a274a79145029f89
Anthony Leandro Vasquez Torrico <vasqueztorricoanthonyleandro@gmail.com>
2026-07-18 15:24:24 -0400

    practicando def

---
+++ 0082.practicando_def.py
@@ -0,0 +1,20 @@
+# def saludar():
+#     print('Hola, Leandro!')
+
+# saludar()
+
+# def saludar_persona(nombre):
+#     print(f'Hola, {nombre}: Como estas?')
+
+# saludar_persona('Carlos')
+# saludar_persona('Baily')
+
+def mostrar_nombre(tu_nombre):
+    print(f'Tu nombre es: {tu_nombre}')
+nombre_de_usuario = input('Ingresa tu nombre: ')
+mostrar_nombre(nombre_de_usuario)
+
+edad = input('Ingresa tu edad: ')
+print(f'Tu edad es: {edad}')
+
+mostrar_nombre(nombre_de_usuario)
