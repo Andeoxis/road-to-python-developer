f2f6f14e752c761322c5b4897d9ee1a5b2fab14f
Anthony Leandro Vasquez Torrico <vasqueztorricoanthonyleandro@gmail.com>
2026-08-01 06:52:54 -0400

    practicando def

---
+++ 0104.de_bolivianos_a_dolares.py
@@ -0,0 +1,6 @@
+def bolivianos_a_dolares(monto_bs):
+    resultado_de_bolivianos_a_dolares = monto_bs * 6.96
+    return resultado_de_bolivianos_a_dolares
+
+respuesta_definitiva = bolivianos_a_dolares(100)
+print(f'La transformacion de bs a dolares es: {respuesta_definitiva}')
