b8039755fb855bcbbca508ba3c9205a2237c1746
Anthony Leandro Vasquez Torrico <vasqueztorricoanthonyleandro@gmail.com>
2026-09-16 23:43:28 -0400

    aprendiendo

--- Manejando_uniones/0001/cliente.py
+++ Manejando_uniones/0001/cliente.py
@@ -2,3 +2,8 @@ def menu_cliente():
     print('MENU DE CLIENTE')
     print('1. Producto')
     print('2. Salir')
+
+def menu():
+    print('Seleccione algun producto:')
+    print('1. Huevo')
+    print('2. Pollo')
